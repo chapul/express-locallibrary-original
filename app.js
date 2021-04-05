1920804d8a87477b43dbbc0b3acb0b1062531edd
chapul <s.espinosa1978@gmail.com>
2021-04-05 15:45:06 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var mongoose = require('mongoose');
 var dev_db_url = 'mongodb+srv://mandril:mandril@cluster0.flfcz.mongodb.net/local_library?retryWrites=true';
 var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
